988ecfe1a159c4fc634cbd47bb67b44be42370c1
ferdjacq <ferdquinjacq@gmail.com>
2026-03-23 17:16:13 +0800

    Build v2

--- public/build/assets/API-c8274bef.js
+++
@@ -1,1 +0,0 @@
-import{l as a}from"./app-10954a3c.js";const o=a.create({baseURL:"/"});export{o as a};
---
+++ public/build/assets/API-d66242e1.js
@@ -0,0 +1,1 @@
+import{l as a}from"./app-59ad18c5.js";const o=a.create({baseURL:"/"});export{o as a};
